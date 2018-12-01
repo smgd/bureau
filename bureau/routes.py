b0350cbd10c9a53c3cfbb81009f61cbece8c6eb9
smgd <soapmangoesdown@gmail.com>
2018-12-01 16:24:11 +0300

    input created

--- bureau/routes.py
+++ bureau/routes.py
@@ -1,21 +1,22 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 import os
+from datetime import datetime
 
 from bureau import app, db
 from bureau.models import Car
-from bureau.forms import SubmitForm
-
-# print(app.config['UPLOAD_FOLDER'])
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = SubmitForm(request.form)
-    
-    if request.method == 'POST' and form.validate():
-        print(app.config['UPLOAD_FOLDER'])
-        filename = form.scan.data.filename
-        f = form.scan.data
-        filename = secure_filename(f.filename)
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    if request.method == 'POST':
+        scan = request.files['scan']
+        vin = request.form['vin']
+        creation_date = datetime.strptime(request.form['creation_date'], '%Y-%m-%d')
+        disposal_date = datetime.strptime(request.form['disposal_date'], '%Y-%m-%d')
+        scan.save(os.path.join(app.config['UPLOAD_FOLDER'], f"{vin}.jpg"))
 
-    return render_template('index.html', form=form)
+        car = Car(id=vin, scan=f"{vin}.jpg", creation_date=creation_date, disposal_date=disposal_date)
+        db.session.add(car)
+        db.session.commit()
+        return 'success!'
+
+    return render_template('index.html')
