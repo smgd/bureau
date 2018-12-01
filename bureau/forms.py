536bbe290e33dd08d179eb866c2d6b958ed5236d
smgd <soapmangoesdown@gmail.com>
2018-12-01 18:29:17 +0300

    rm forms

--- bureau/forms.py
+++
@@ -1,8 +0,0 @@
-from wtforms import Form, StringField, DateField, FileField, validators
-from flask_wtf.file import FileRequired
-
-class SubmitForm(Form):
-    vin = StringField('VIN', [validators.Length(min=17, max=17)])
-    scan = FileField('Scan', validators=[FileRequired()])
-    creation_date = DateField('Creation Date', format='%Y-%m-%d')
-    disposal_date = DateField('Disposal Date', format='%Y-%m-%d')
