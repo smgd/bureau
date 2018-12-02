8d5628e6f43056b0ac5fc1649a7ad3d2c6b08c48
smgd <soapmangoesdown@gmail.com>
2018-12-02 12:37:05 +0300

    migrate to postgresql

--- bureau/__init__.py
+++ bureau/__init__.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__, template_folder='../templates')
 
 app.config['SECRET_KEY'] = '123456'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../cars.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres@localhost/cars'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'files')
 
@@ -13,7 +13,7 @@ db = SQLAlchemy(app)
 
 from bureau.models import Car
 
-if not os.path.exists(os.path.join(os.getcwd(), 'cars.db')):
-    db.create_all()
+# if not os.path.exists(os.path.join(os.getcwd(), 'cars.db')):
+#     db.create_all()
 
 from bureau import routes
